87df5ec8e9fb22dcac243b47ba74eef587391c39
Lucas-Vini <lvinicius31@gmail.com>
2025-02-11 15:38:12 -0300

    fixing build max and min heap methods

--- data_structures/max_heap.py
+++ data_structures/max_heap.py
@@ -9,20 +9,15 @@ class MaxHeap(Heap):
         if (left < self.heap_size) and (self.heap[left] > self.heap[i]):
             largest = left
         
-        if (rigth < self.heap_size) and (self.heap[rigth] > self.heap[i]):
+        if (rigth < self.heap_size) and (self.heap[rigth] > self.heap[largest]):
             largest = rigth
 
         if largest != i:
             self.heap[i], self.heap[largest] = self.heap[largest], self.heap[i]
-            self._max_heapify(i)
+            self._max_heapify(largest)
 
     def build_max_heap(self, heap: list):
         self.heap = heap
         self.heap_size = len(heap)
         for i in range(self.heap_size // 2, -1, -1):
             self._max_heapify(i)
-
-my_heap = MaxHeap()
-my_heap.build_max_heap([4, 1, 3, 2, 16, 9, 10, 14, 8, 7])
-
-print(my_heap.heap)
--- data_structures/min_heap.py
+++ data_structures/min_heap.py
@@ -1,17 +1,23 @@
 from heap import Heap
 
 class MinHeap(Heap):
-    def _min_heapify(self, i):
+    def _min_heapify(self, i: int):
         smallest = i
         left = self._left(i)
         rigth = self._right(i)
 
-        if (left <= self.heap_size) and (self.heap[left] < self.heap[i]):
+        if (left < self.heap_size) and (self.heap[left] < self.heap[i]):
             smallest = left
         
-        if (rigth <= self.heap_size) and (self.heap[rigth] < self.heap[i]):
+        if (rigth < self.heap_size) and (self.heap[rigth] < self.heap[smallest]):
             smallest = rigth
 
         if smallest != i:
             self.heap[i], self.heap[smallest] = self.heap[smallest], self.heap[i]
-            self._max_heapify(i)
+            self._min_heapify(smallest)
+
+    def build_min_heap(self, heap: list):
+        self.heap = heap
+        self.heap_size = len(heap)
+        for i in range(self.heap_size // 2, -1, -1):
+            self._min_heapify(i)
