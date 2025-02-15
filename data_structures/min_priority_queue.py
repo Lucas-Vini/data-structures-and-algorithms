af557e23fb8dd3db2b3d69e98039c1f1c0019ec8
Lucas-Vini <lvinicius31@gmail.com>
2025-02-15 16:06:31 -0300

    min priority queue

--- data_structures/min_priority_queue.py
+++ data_structures/min_priority_queue.py
@@ -1,29 +1,29 @@
-from data_structures.max_heap import MaxHeap
+from data_structures.min_heap import MinHeap
 
-class MaxPriorityQueue(MaxHeap):
-    def heap_maximum(self):
+class MinPriorityQueue(MinHeap):
+    def heap_minimum(self):
         return self.heap[0]
     
-    def heap_extract_max(self):
+    def heap_extract_min(self):
         if self.heap_size < 1:
             raise Exception("Heap underflow.")
         
-        max = self.heap[0]
+        min = self.heap[0]
         self.heap[0] = self.heap[self.heap_size - 1]
         self.heap_size -= 1
-        self.max_heapify(0)
-        return max
+        self.min_heapify(0)
+        return min
 
-    def heap_increase_key(self, i: int, key):
-        if key < self.heap[i]:
+    def heap_decrease_key(self, i: int, key):
+        if key > self.heap[i]:
             raise Exception("New key is smaller than current key")
         
         self.heap[i] = key
-        while i > 0 and self.heap[self._parent(i)] < self.heap[i]:
+        while i > 0 and self.heap[self._parent(i)] > self.heap[i]:
             self.heap[self._parent(i)], self.heap[i] = self.heap[i], self.heap[self._parent(i)]
             i = self._parent(i)
 
-    def max_heap_insert(self, key):
+    def min_heap_insert(self, key):
         self.heap_size += 1
         self.heap[self.heap_size - 1] = key
-        self.heap_increase_key(self.heap_size - 1, key)
+        self.heap_decrease_key(self.heap_size - 1, key)
