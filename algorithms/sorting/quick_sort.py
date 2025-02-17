f079511ee2f6044e514e722e4dd357f2d9071c3a
Lucas-Vini <lvinicius31@gmail.com>
2025-02-17 14:50:28 -0300

    inclugin reverse option in quick sort

--- algorithms/sorting/quick_sort.py
+++ algorithms/sorting/quick_sort.py
@@ -1,17 +1,17 @@
-def quick_sort(elements: list, start=0, end=None) -> list:
+def quick_sort(elements: list, start=0, end=None, reverse=False) -> list:
     if end is None:
         end = len(elements) - 1
 
     if start < end:
-        pivot = partition(elements, start, end)
-        quick_sort(elements, start, pivot - 1)
-        quick_sort(elements, pivot + 1, end)
+        pivot = partition(elements, start, end, reverse=reverse)
+        quick_sort(elements, start, pivot - 1, reverse=reverse)
+        quick_sort(elements, pivot + 1, end, reverse=reverse)
 
-def partition(elements, start, end):
+def partition(elements, start, end, reverse):
     pivot = elements[end]
     i = start - 1
     for j in range(start, end):
-        if elements[j] <= pivot:
+        if (elements[j] <= pivot and reverse == False) or (elements[j] >= pivot and reverse):
             i += 1
             elements[i], elements[j] = elements[j], elements[i]
     elements[i+1], elements[end] = elements[end], elements[i+1]
