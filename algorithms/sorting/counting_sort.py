837e3ceb894aa8c0d59258ec2575eb13958d0718
Lucas-Vini <lvinicius31@gmail.com>
2025-02-19 22:19:09 -0300

    counting sort

--- algorithms/sorting/counting_sort.py
+++ algorithms/sorting/counting_sort.py
@@ -1,2 +1,18 @@
-def counting_sort(elements: list):
-    pass
+def counting_sort(elements: list): 
+    largest_value = max(elements)
+    counter_values = [0] * (largest_value + 1)
+
+    for i in range(len(elements)):
+        counter_values[elements[i]] += 1
+
+    elements_counter = 0
+    for i in range(len(counter_values)):
+        while counter_values[i] > 0:
+            elements[elements_counter] = i
+            counter_values[i] -= 1
+            elements_counter += 1
+    return elements
+
+a = [5, 9, 1, 8, 2, 7, 3, 6, 4, 10, 10, 191, 10, 0 , 0, 0, 0, 0, 0, 0, 0, 0]
+print(counting_sort(a))
+    
