e084716b08b87d577bd3a16671a661117ec4426e
Lucas-Vini <lvinicius31@gmail.com>
2025-02-27 22:11:56 -0300

    append method in linked list

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -1,4 +1,16 @@
+from node import Node
+
 class LinkedList:
     def __init__(self):
-        self.head = None
+        self.first = None
+        self.last = None
         self.length = 0
+
+    def append(self, value):
+        if self.first:
+            self.last.next = Node(value)
+            self.last = self.last.next
+        else:
+            self.first = Node(value)
+            self.last = self.first
+        self.length += 1
