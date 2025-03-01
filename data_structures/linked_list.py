8cb311932f3fbc88ecc02b58624abbea1f9b5b9e
Lucas-Vini <lvinicius31@gmail.com>
2025-03-01 15:14:21 -0300

    linked list get item method

--- data_structures/linked_list.py
+++ data_structures/linked_list.py
@@ -4,7 +4,7 @@ class LinkedList:
     def __init__(self):
         self.first = None
         self.last = None
-        self.length = 0
+        self._length = 0
 
     def append(self, value):
         if self.first:
@@ -13,7 +13,17 @@ class LinkedList:
         else:
             self.first = Node(value)
             self.last = self.first
-        self.length += 1
+        self._length += 1
 
     def __len__(self):
-        return self.length
+        return self._length
+    
+    def __getitem__(self, index):
+        pointer = self.first
+        for i in range(index):
+            if not pointer.next:
+                raise IndexError("list index out of range")
+            pointer = pointer.next
+        if not pointer:
+            raise IndexError("list index out of range")
+        return pointer.value
