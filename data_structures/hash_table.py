5e62878d17dab810d7dbce544dfa59dea26ff47c
Lucas-Vini <lvinicius31@gmail.com>
2025-05-20 21:30:58 -0300

    hash table part 2

--- data_structures/hash_table.py
+++ data_structures/hash_table.py
@@ -2,3 +2,7 @@ class HashTable:
     def __init__(self, size):
         self.size = size
         self.table = [[] for _ in range(size)]
+
+    def _hash(self, key):
+        return hash(key) % self.size
+    
