2a1cbd05ea4550c33206b545b6d06b518796ac59
Lucas-Vini <lvinicius31@gmail.com>
2025-05-21 19:51:58 -0300

    hash table part 3

--- data_structures/hash_table.py
+++ data_structures/hash_table.py
@@ -6,3 +6,26 @@ class HashTable:
     def _hash(self, key):
         return hash(key) % self.size
     
+    def set(self, key, value):
+        index = self._hash(key)
+
+        for i in range(len(self.table[index])):
+            if self.table[index][i][0] == key:
+                self.table[index][i] = (key, value)
+                return
+
+        self.table[index].append((key, value))
+
+    def get(self, key):
+        index = hash(key) % self.size
+
+        for k, v in self.table[index]:
+            if key == k:
+                return v
+
+df = HashTable(3)
+df.set("casa", "carro")
+df.set("agua", "fogo")
+print(df.get("agua"))
+print(df.get("casa"))
+print(df.get("ca"))
