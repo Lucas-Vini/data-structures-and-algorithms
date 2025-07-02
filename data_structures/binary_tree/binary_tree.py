3e4d171a40026f13ad413326c6ae1cd44c2278da
Lucas-Vini <lvinicius31@gmail.com>
2025-07-01 22:24:17 -0300

    binary tree class

--- data_structures/binary_tree/binary_tree.py
+++ data_structures/binary_tree/binary_tree.py
@@ -3,3 +3,10 @@ class TreeNode:
         self.value = value
         self.left = None
         self.right = None
+
+class BinaryTree:
+    def __init__(self, value=None):
+        self.root = None
+        if value:
+            root_node = TreeNode(value)
+            self.root = root_node
