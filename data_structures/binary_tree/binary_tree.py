1c02ffccdbd3ca59dcfab461788ec3bea1a30d98
Lucas-Vini <lvinicius31@gmail.com>
2025-07-06 20:32:33 -0300

    simetric traversal

--- data_structures/binary_tree/binary_tree.py
+++ data_structures/binary_tree/binary_tree.py
@@ -10,3 +10,40 @@ class BinaryTree:
         if value:
             root_node = TreeNode(value)
             self.root = root_node
+
+    def simetric_traversal(self, node=None):
+        if node is None:
+            node=self.root
+        
+        if node.left:
+            print('(', end='')
+            self.simetric_traversal(node.left)
+        print(node.value, end='')
+        if node.right:
+            self.simetric_traversal(node.right)
+            print(')', end='')
+
+
+my_tree = BinaryTree()
+
+n1 = TreeNode('a')
+n2 = TreeNode('+')
+n3 = TreeNode('*')
+n4 = TreeNode('b')
+n5 = TreeNode('-')
+n6 = TreeNode('/')
+n7 = TreeNode('c')
+n8 = TreeNode('d')
+n9 = TreeNode('e')
+
+n6.left = n7
+n6.right = n8
+n5.left = n6
+n5.right = n9
+n3.left = n4
+n3.right = n5
+n2.left = n1
+n2.right = n3
+my_tree.root = n2
+
+my_tree.simetric_traversal()
